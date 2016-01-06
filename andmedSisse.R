7ec758d797a498835112ee1f06627cc769834782
Risto Hinno <ristohinno@gmail.com>
2016-01-06 14:12:49 +0200

    andmedSisse lisasin mõõtmise aasta tulemisse

--- andmedSisse.R
+++ andmedSisse.R
@@ -19,7 +19,7 @@ meltimine=function(kanal, data) {
 }
 
 #abifunktsioon andmete sisse lugemiseks
-korrastaja=function(andmed, eemalda) {
+korrastaja=function(andmed, eemalda, mootmiseAasta) {
   library(reshape2)
   #eemalda - mis osa columnite nimedest tuleb eemdalda
   names(andmed)=gsub(pattern=eemalda,"" ,names(andmed))
@@ -62,6 +62,7 @@ korrastaja=function(andmed, eemalda) {
   koos$naitaja=df[,3]
   #teeme numbriliseks
   koos$value=as.numeric(as.character(koos$value))
+  koos$MootmiseAasta=mootmiseAasta
   koos
 } 
 #andmete download
@@ -97,12 +98,12 @@ DataLong2=function(andmedLai) {
   andmedLai2012=andmedLai[, !grepl("empty.|2011.|2013.|2014.|2015.", names(andmedLai))]
   andmedLai2011=andmedLai[, !grepl("empty.|2014.|2013.|2012.|2015.", names(andmedLai))]
   andmedLaiEmpty=andmedLai[, !grepl("2014.|2011.|2013.|2012.|2015.", names(andmedLai))]
-  puhas2015=korrastaja(andmedLai2015, "2015.")
-  puhas2014=korrastaja(andmedLai2014, "2014.")
-  puhas2013=korrastaja(andmedLai2013, "2013.")
-  puhas2012=korrastaja(andmedLai2012, "2012.")
-  puhas2011=korrastaja(andmedLai2011, "2011.")
-  puhasEmpty=korrastaja(andmedLaiEmpty, "empty.")
+  puhas2015=korrastaja(andmedLai2015, "2015.", "2015")
+  puhas2014=korrastaja(andmedLai2014, "2014.", "2014")
+  puhas2013=korrastaja(andmedLai2013, "2013.", "2013")
+  puhas2012=korrastaja(andmedLai2012, "2012.", "2012")
+  puhas2011=korrastaja(andmedLai2011, "2011.", "2011")
+  puhasEmpty=korrastaja(andmedLaiEmpty, "empty.", "pole moodetud")
   #paneme andmed kokku
   andmedPikk=rbind(puhas2015, puhas2014,puhas2013,puhas2012, puhas2011,puhasEmpty)
   #eemaldame read, kus pole linki
